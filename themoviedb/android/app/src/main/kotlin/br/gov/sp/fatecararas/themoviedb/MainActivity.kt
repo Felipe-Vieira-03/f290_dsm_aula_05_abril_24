1d4ed48edcb0bfa45fe82d995b698ac87f19b260
Felipe-Vieira-03 <felipedpvs@hotmail.com>
2024-04-06 01:44:09 -0300

    Adicionando primeiros passos

---
+++ themoviedb/android/app/src/main/kotlin/br/gov/sp/fatecararas/themoviedb/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.gov.sp.fatecararas.themoviedb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ themoviedb/lib/repositories/movie_repository.dart
@@ -0,0 +1,6 @@
+import 'package:themoviedb/model/movie_model.dart';
+
+abstract class MovieRepository {
+  Future<List<MovieModel>> getUpcoming();
+  Future<bool> addRating(String id, double rate);
+}
